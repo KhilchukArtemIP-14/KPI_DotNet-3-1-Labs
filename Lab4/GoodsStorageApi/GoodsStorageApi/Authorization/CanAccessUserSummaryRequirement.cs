8756680991f460c568ed537f68d0a1d460f9ec32
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2023-12-20 17:13:16 +0200

    Revert "Lab 4"

--- Lab4/GoodsStorageApi/GoodsStorageApi/Authorization/CanAccessUserSummaryRequirement.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Authorization;
-
-namespace GoodsStorage.API.Authorization
-{
-    public class CanAccessUserSummaryRequirement: IAuthorizationRequirement
-    {
-    }
-}
